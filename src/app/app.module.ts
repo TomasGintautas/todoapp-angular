4c93d1f29356152af449608b6345b30c1f236cfb
TomasGintautas <tomasgintaut@gmail.com>
2021-08-22 12:39:20 +0300

    initial project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserViewComponent } from './user-view/user-view.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserViewComponent,
+    UserEditComponent,
+    WorkspaceComponent,
+    RegisterComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
